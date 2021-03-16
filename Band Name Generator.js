68ce5ed23c6f0da702733dbca1c3f93c1759ca64
plippitt1 <plippitt1@gmail.com>
2021-03-16 14:09:25 +0000

    Store name components

--- Band Name Generator.js
+++ Band Name Generator.js
@@ -1,1 +1,3 @@
-console.log('Hello World!');
+const firstWords = ['The', 'Big', 'Small', 'Shiny', 'Angry', 'Hungry', 'Iron', 'Confused', 'Electronic', 'Molten'];
+const secondWords = ['Chocolate', 'Midnight', 'Golden', 'Little', 'Rusty', 'Hungry', 'Flying', 'Musical', 'Super', 'Psychic'];
+const thirdWords = ['Lobsters', 'Rhinos', 'Killers', 'Radiators', 'Robots', 'Spiders', 'Roses', 'Ladies', 'Raspberries', 'Monkeys'];
